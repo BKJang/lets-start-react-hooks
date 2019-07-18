c82f596d420220ce984daeb46bfebffb0049603d
BKJang <jp302119@gmail.com>
2019-07-18 23:45:25 +0900

    useEffect 샘플 구현

--- src/User.js
+++ src/User.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function User({ user, handleRemoveElement, handleToggleElement }) {
   const { id, userName, email, active } = user;
+  useEffect(() => {
+    console.log('컴포넌트 마운트 : 값이 바뀌거나 초기 값 설정시 동작');
+    console.log(user);
+    // props => state 세팅
+    // REST API
+    // 외부 라이브러리(D3, Video)
+    // setInterval, setTimeout
+    return () => {
+      // clearInterval, clearTimeout
+      // 라이브러리 인스턴스 제거
+      console.log('컴포넌트 언마운트 : 값이 바뀌기 직전');
+      console.log(user);
+    }
+  }, [user]);
   return (
     <div>
       <b
