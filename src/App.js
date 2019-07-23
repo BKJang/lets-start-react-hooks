f6179cc2b721884068854b17907bb9e61ada1a12
BKJang <jp302119@gmail.com>
2019-07-24 01:34:39 +0900

    useReducer를 사용하여 App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useMemo, useCallback } from 'react';
+import React, { useRef, useReducer, useMemo, useCallback } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 
@@ -7,22 +7,12 @@ function countActiveUsers(users) {
   return users.filter(user => user.active).length;
 }
 
-function App() {
-  const [input, setInput] = useState({
+const initialState = {
+  inputs: {
     userName: '',
     email: '',
-  });
-  const { userName, email } = input;
-
-  const handleChangeElement = useCallback(e => {
-    const { name, value } = e.target;
-    setInput({
-      ...input,
-      [name]: value,
-    });
-  }, [input]);
-
-  const [users, setUsers] = useState([
+  },
+  users: [
     {
       id: 1,
       userName: 'BKJang',
@@ -41,51 +31,106 @@ function App() {
       email: 'cdf@example.com',
       active: false,
     }
-  ]);
+  ]
+}
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'CHANGE_INPUT': {
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.name]: [action.value]
+        }
+      }
+    }
+    case 'CREATE_USER': {
+      return {
+        inputs: initialState.inputs,
+        users: [...state.users, action.user]
+      }
+    }
+    case 'TOGGLE_USER': {
+      return {
+        ...state,
+        users: state.users.map(user => {
+          return user.id === action.id
+            ? { ...user, active: !user.active }
+            : user
+        })
+      }
+    }
+    case 'REMOVE_USER': {
+      return {
+        ...state,
+        users: state.users.filter(user => {
+          return user.id !== action.id;
+        })
+      }
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
   const nextId = useRef(4);
+  const { users } = state;
+  const { userName, email } = state.inputs;
+
+  const handleChangeElement = useCallback(e => {
+    const { name, value } = e.target;
+    dispatch({
+      type: 'CHANGE_INPUT',
+      name,
+      value
+    })
+  }, []);
 
   const handleCreateElement = useCallback(() => {
-    const user = {
-      id: nextId.current,
-      userName,
-      email,
-    }
-    setUsers(users => [
-      ...users,
-      user
-    ])
-    setInput({
-      userName: '',
-      email: '',
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id: nextId.current,
+        userName,
+        email,
+      }
     })
+
     nextId.current += 1;
   }, [userName, email])
 
-  const handleRemoveElement = useCallback(userId => {
-    setUsers(users => users.filter(user => {
-      return user.id !== userId;
-    }))
-  }, []);
-
   const handleToggleElement = useCallback(id => {
-    setUsers(users =>
-      users.map(user =>
-        user.id === id ? { ...user, active: !user.active } : user
-      )
-    );
+    dispatch({
+      type: 'TOGGLE_USER',
+      id
+    });
   }, []);
 
-  const count = useMemo(() => countActiveUsers(users), [users]);
+
+  const handleRemoveElement = useCallback(id => {
+    dispatch({
+      type: 'REMOVE_USER',
+      id
+    });
+  }, []);
+
+  const count = useMemo(() => {
+    return countActiveUsers(users);
+  }, [users])
+
   return (
     <>
-      <CreateUser
-        userName={userName}
-        email={email}
+      <CreateUser 
+        userName={userName} 
+        email={email} 
         handleChangeElement={handleChangeElement}
         handleCreateElement={handleCreateElement}
       />
-      <UserList users={users} handleRemoveElement={handleRemoveElement} handleToggleElement={handleToggleElement} />
+      <UserList users={users} handleToggleElement={handleToggleElement} handleRemoveElement={handleRemoveElement}/>
       <div>활성 사용자 수 : {count}</div>
     </>
   );
